65fdbf36e8eb3a535d022458612f47ca0f0b30aa
redebroi <rene-paul.debroize@inria.fr>
2024-01-04 14:37:21 +0100

    Fix public definitions, add messages during cmake configuration.

--- xxx/lib/bar/src/bar.cpp
+++ xxx/lib/bar/src/bar.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 void xxx::bar::Bar::trigger() {
+    std::cout << "XXX macro is: " << XXX <<  std::endl;
     std::cout << "trigerring xxx::bar." << std::endl;
     this->foo.trigger();
 }
